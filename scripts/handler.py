fab3cc6aa1fd0656b58a58c8283256a29b3a4c73
Daniel Porteous <danielporteous1@gmail.com>
2016-03-20 10:01:08 +1100

    Didn't work?
    Could have also been string rearrangement

--- scripts/handler.py
+++ scripts/handler.py
@@ -58,7 +58,7 @@ def getSupplierGrid(dbObj):
         <div class="column">
         <img class="thumbnail" width=450px height=450px src="%s">
         <h5 style="text-align:center">%s</h5></div> """ % (item[1], item[0])
-        print(square*7)
+        print(square)
 
 def getSupplierName(dbObj):
     print("Hi")
