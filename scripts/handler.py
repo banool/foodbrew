65f7c727de689a08055b0a791a37500b6caa4161
Daniel Porteous <danielporteous1@gmail.com>
2016-03-20 08:29:09 +1100

    Finally consistent space indentation
    Hope this works.

--- scripts/handler.py
+++ scripts/handler.py
@@ -9,38 +9,38 @@ MYSQL_pwd="pass1234"
 print("Content-Type: text/html\n")
 
 def entryPoint():
-	form = cgi.FieldStorage()
-	print(form)
-	
-	db = MySQLdb.connect(host="localhost", user="daniel", passwd=MYSQL_pwd, db="fft")
-	dbObj = db.cursor()
+    form = cgi.FieldStorage()
+    print(form)
+    
+    db = MySQLdb.connect(host="localhost", user="daniel", passwd=MYSQL_pwd, db="fft")
+    dbObj = db.cursor()
 
     # Ascertaining what action we're meant to perform
     try:
-    	action = form["action"].value
-    	if action == "loginForm":
-    		loginForm(dbObj)
-    	elif action == "getTypes":
-    		getTypes()
-    	elif action == "formData":
-    		info = prepareInfo(dbObj, form)
-    		if info == False:
+        action = form["action"].value
+        if action == "loginForm":
+            loginForm(dbObj)
+        elif action == "getTypes":
+            getTypes()
+        elif action == "formData":
+            info = prepareInfo(dbObj, form)
+            if info == False:
                 # Exiting if it didn't work properly.
                 exit()
             else:
-            	makeQuery(dbObj, info)
+                makeQuery(dbObj, info)
 
             else:
-            	print("An unexpected request was received, exiting.")
-            	exit()
+                print("An unexpected request was received, exiting.")
+                exit()
     except KeyError:
-    	print("<h1>Woops!</h1>")
-    	print("<p>Looks like you've come straight to this page without actually making an entry!</p>")
-    	print("<p>Go back to the <a href='Insert Page and try again :)")
+        print("<h1>Woops!</h1>")
+        print("<p>Looks like you've come straight to this page without actually making an entry!</p>")
+        print("<p>Go back to the <a href='Insert Page and try again :)")
 
-    	db.commit()
+        db.commit()
 
-    	def loginForm():
-    		print("Hey IT'S ME")
+        def loginForm():
+            print("Hey IT'S ME")
 
-    		entryPoint()
+            entryPoint()
