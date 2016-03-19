90bfa70adf6f3995f1de614b67c63242bf2d5281
Daniel Porteous <danielporteous1@gmail.com>
2016-03-20 10:06:24 +1100

    Can't seem to get the modal to work just right now.
    Will look into the full page set up. Need to pass variable in url so we
    know what supplier page to generate.

--- scripts/handler.py
+++ scripts/handler.py
@@ -36,14 +36,6 @@ def entryPoint():
 
         db.commit()
 
-          <div class="column">
-          <a data-open="exampleModal1">
-            <img class="thumbnail" src="http://placehold.it/550x550">
-            <h5><div id="supplierGridTarget"></div></h5>
-          </a>
-          </div>
-
-
 def loginForm(dbObj):
     print("Hey IT'S ME!!")
 
@@ -53,7 +45,7 @@ def getSupplierGrid(dbObj):
     userData = dbObj.fetchall()
 
     for item in userData:
-        square = """<div class="column"><a data-open="exampleModal1"><div class="column"><img class="thumbnail" width=450px height=450px src="%s"><h5 style="text-align:center">%s</h5></div></a></div> """ % (item[1], item[0])
+        square = """<div class="column"><img class="thumbnail" width=450px height=450px src="%s"><h5 style="text-align:center">%s</h5></div> """ % (item[1], item[0])
         print(square*7)
 
 def getSupplierName(dbObj):
