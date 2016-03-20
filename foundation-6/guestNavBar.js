26a53c17691e8b5dcd7884f90329d0ca1364cf91
Daniel Porteous <danielporteous1@gmail.com>
2016-03-20 12:29:41 +1100

    Recompiled potential fix

--- foundation-6/guestNavBar.js
+++ foundation-6/guestNavBar.js
@@ -1,11 +1,5 @@
 document.write('\
 \
-  <link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">\
-  <link rel="stylesheet" href="css/app.css" />\
-  <link href='http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css' rel='stylesheet' type='text/css'>\
-\
-\
-\
 <style>\
 a {\
   color: #333333;;\
@@ -96,7 +90,6 @@ a {\
 \
   <!-- Scripts -->\
 \
-  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>\
       <script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>\
   \
   <script>\
